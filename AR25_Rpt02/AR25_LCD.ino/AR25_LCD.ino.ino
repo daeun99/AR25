cbd90a3ecb17af7655c3e9b0007912f3647b1ae2
yorsing <48310109+yorsing@users.noreply.github.com>
2019-09-26 16:50:48 +0900

    wk03_src

---
+++ AR25_Rpt02/AR25_LCD.ino/AR25_LCD.ino.ino
@@ -0,0 +1,68 @@
+// I2C 통신 라이브러리 설정
+#include <Wire.h>
+// I2C LCD 라리브러리 설정
+#include <LiquidCrystal_I2C.h>
+
+int blinkNumber=0;
+
+// LCD I2C address 설정 PCF8574:0x27, PCF8574A:0x3F
+LiquidCrystal_I2C lcd(0x27,16,2);  // LCD address:0x27, 16X2 LCD, 0x3F
+
+void setup()
+{
+  // 9600 bps로 시리얼 통신 설정
+  Serial.begin(9600);
+  lcd.init(); // LCD 설정
+  lcd.clear();      // LCD를 모두 지운다.
+  lcd.backlight();  // 백라이트를 켠다.  
+  // Arduino LCD, Welcome 표시  
+  lcd.setCursor(0,0);
+  lcd.print("Arduino LCD");
+  delay(3000);
+  lcd.setCursor(0,1);
+  lcd.print("Welcome");
+  delay(250);  
+lcd.noBacklight();
+  delay(250);
+  lcd.backlight();
+  delay(250);
+
+}
+
+void loop()
+{
+  // 시리얼 통신 수신 값이 있을 때
+  if (Serial.available()) {  
+    delay(100);
+    // 모두 삭제
+    lcd.clear();
+     // 커서를 좌측 상단으로
+    lcd.setCursor(0,0);
+    // "Message from PC" 출력
+    lcd.print("Insert to Number");
+    // 커서를 두 번째 줄로
+    lcd.setCursor(0,1);
+    
+
+    // LCD에 PC에서 전송된 데이터를 출력
+    while (Serial.available()>0) {
+      char val = Serial.read();
+      blinkNumber = (val - '0');
+      if(isDigit(val))
+      {
+        lcd.print("Enter : ");
+        lcd.print(val);
+        for(int i = 0; i<blinkNumber; i++)
+        {
+          lcd.noBacklight();
+          delay(100);
+          lcd.backlight();
+          delay(100);
+        }
+        delay(5000);
+        lcd.clear();
+  
+      }
+    }
+  }
+}
---
+++ AR25_Rpt02/AR25_number.ino/AR25_number.ino.ino
@@ -0,0 +1,39 @@
+/*
+ AR25_number.ino
+ */
+
+int blinkNumber=0;
+
+// 문자열 두가지를 설정한다.
+String stringValue[]={"Binary:", "Octal: "}; // array
+
+void setup() {
+  // 9600bps로 시리얼 통신 설정
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(Serial.available()){
+    char val =Serial.read();
+    if(isDigit(val)){
+      blinkNumber=(val-'0');
+    }
+  }
+  if(blinkNumber != 0)
+  {
+   // 'char Value'를 출력하고 문자열과 숫자를 변수 유형별로 출력한다.
+  Serial.print("Number = ");
+  Serial.print(blinkNumber);
+  Serial.print(", ");
+  Serial.print(stringValue[0]);  // stringValue 중 첫 번째 문자열 출력
+  Serial.print(blinkNumber,BIN); // 2진수 형태로 출력
+  Serial.print(", ");
+  Serial.print(stringValue[1]);  // stringValue 중 첫 번째 문자열 출력
+  Serial.print(blinkNumber,OCT); // 16진수 형태로 출력
+  // 줄바꿈
+  Serial.println();
+  }
+
+  //점멸 횟수를 리셋함
+  blinkNumber=0;
+}
